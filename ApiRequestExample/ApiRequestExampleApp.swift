e0da5578c8476caaef64ae042b89c4e624f0c27b
José Winny <jose.winny.dev@gmail.com>
2023-12-05 14:12:13 -0300

    Initial Commit

---
+++ ApiRequestExample/ApiRequestExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiRequestExampleApp.swift
+//  ApiRequestExample
+//
+//  Created by José Winny on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ApiRequestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
